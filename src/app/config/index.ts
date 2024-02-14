273ba1a2877fc145436538ade2adbf2d0e1a5dab
Sunil Bagde <bagdesunil786@gmail.com>
2024-02-15 00:19:58 +0530

    fix: imp

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,3 +1,2 @@
 export const API_URL= process.env.API_URL || `https://jsonplaceholder.typicode.com`
 export const SITE_URL= process.env.SITE_URL || `http://localhost:3000`
-export const SITE_URL= process.env.JWT_SECRET_KEY || `12312`
